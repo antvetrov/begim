06c79e0eb8338bf4632dfa7d760d868f2192bf7a
Anton Vetrov <ant.vetrov@gmail.com>
2015-12-06 11:35:25 +0000

    fix auth bug

--- app/menu/menu.js
+++ app/menu/menu.js
@@ -12,14 +12,14 @@ angular.module('myApp.menu', [])
 	// any time auth status updates, add the user data to scope
   auth.$onAuth(function(authData) {
     $scope.authData = authData;
+
+    //Todo list
+    var today = new Date().getDay();
+    var userId = FirebaseUrl.getAuth().uid;
+    var weekdays = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+    $scope.trainingPlan = $firebaseArray(FirebaseUrl.child("users/" + userId + 
+      "/trainingPlan/" + weekdays[today]));
   });
-  
-  //Todo list
-  var today = new Date().getDay();
-  var userId = FirebaseUrl.getAuth().uid;
-  var weekdays = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-  $scope.trainingPlan = $firebaseArray(FirebaseUrl.child("users/" + userId + 
-    "/trainingPlan/" + weekdays[today]));
 
  	//Login with facebook
  	$scope.loginFacebook = function() {
